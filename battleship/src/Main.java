1031f391a5f4f4f733642224a64437cad8ce7144
Maksym Korniiko <korniykom@gmail.com>
2024-09-28 16:04:08 +0300

    fead: add ship placement w/o error handling

--- battleship/src/Main.java
+++ battleship/src/Main.java
@@ -1,8 +1,68 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+
+
 public class Main {
     public static void main(String[] args) {
         char[][] field = new char[10][10];
+
+        HashMap<String, Integer> ships = new HashMap<>();
+        ships.put("Aircraft Carrier", 5);
+        ships.put("Battleship", 4);
+        ships.put("Submarine", 3);
+        ships.put("Cruiser", 3);
+        ships.put("Destroyer", 2);
+
+        
+        Scanner scanner = new Scanner(System.in);
+
+
         fillField(field);
         printField(field);
+        placeShips(scanner, field, ships);
+
+
+
+        scanner.close();
+    }
+
+    public static void placeShips(Scanner scanner, char[][] field, HashMap<String, Integer> ships) {
+        String[] shipsNames = {"Aircraft Carrier","Battleship","Submarine","Cruiser","Destroyer"};
+
+        int shipPlaced = 0;
+        while(true) {
+            System.out.printf("\nEnter the coordinates of the %s (%d cells): ", shipsNames[shipPlaced], ships.get(shipsNames[shipPlaced]));
+            String input = scanner.nextLine();
+            String[] coordinates = input.split(" ");
+
+            placeShip(coordinates[0], coordinates[1], field);
+            printField(field);
+            shipPlaced++;
+            if(shipPlaced >= shipsNames.length) {
+                break;
+            }
+
+        }
+    }
+
+    public static void placeShip(String firstCoordinate, String secondCoordinate,char[][] field) {
+
+            if (firstCoordinate.charAt(0) == secondCoordinate.charAt(0)) {
+                int row = firstCoordinate.charAt(0) - 'A';
+                for (int col = Integer.parseInt(firstCoordinate.substring(1)) - 1; col <= Integer.parseInt(secondCoordinate.substring(1)) - 1; col++) {
+                    field[row][col] = 'O';
+                }
+            } else if (Integer.parseInt(firstCoordinate.substring(1)) == Integer.parseInt(secondCoordinate.substring(1))) {
+                int col = Integer.parseInt(firstCoordinate.substring(1)) - 1;
+                for (int row = firstCoordinate.charAt(0) - 'A'; row <= secondCoordinate.charAt(0) - 'A'; row++) {
+                    field[row][col] = 'O';
+                }
+            } else {
+                System.out.println("Error!");
+            }
+
+
     }
 
     public static void fillField(char[][] field) {
@@ -14,8 +74,9 @@ public class Main {
     }
 
     public static void printField(char[][] field) {
+        System.out.println();
         System.out.print("  ");
-        for(int i = 0; i < field.length; i++) {
+        for(int i = 1; i <= field.length; i++) {
             System.out.printf("%d ", i);
         }
         System.out.println();
